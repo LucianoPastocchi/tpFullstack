b1b338025d33d1711901dcb4edbfb02e7d4a1f17
System Administrator <root@MacBook-Air.local>
2023-09-23 23:54:34 -0300

    characters controller and model

---
+++ Backend/controllers/characterController.js
@@ -0,0 +1,80 @@
+require("mongoose");
+const Character = require("../models/charaterModel");
+const Usr = require("../models/userModel");
+
+const getAllCharacters = async (limit, offset) => {
+  const characters = await Character.find({}).limit(limit).skip(offset);
+
+  return characters;
+};
+
+const getCharacter = async (id) => {
+  const character = await Character.findById(id);
+  return character;
+};
+
+const getCharactersByUser = async (userId) => {
+  const user = await Usr.findById(userId);
+
+  if (!user) {
+    return false;
+  } else {
+    const characters = await Character.find({ createdBy: userId });
+
+    return characters;
+  }
+};
+
+const createCharacter = async (
+  userId,
+  characterID,
+  name,
+  faceImage,
+  upperBody,
+  lowerBody,
+  shoes
+) => {
+  const user = await Usr.findById(userId);
+
+  if (!user) {
+    return false;
+  } else {
+    const character = new Character({
+      characterID: characterID,
+      name: name,
+      faceImage: faceImage,
+      upperBody: upperBody,
+      lowerBody: lowerBody,
+      shoes: shoes,
+      createdBy: userId,
+    });
+
+    let createdCharacter = await character.save();
+    console.log("Personaje nuevo");
+    console.log(createdCharacter);
+    return { createdCharacter };
+  }
+};
+
+const editCharacter = async (character) => {
+  const result = await Character.findByIdAndUpdate(character._id, character, {
+    new: true,
+  });
+
+  return result;
+};
+
+const deleteCharacter = async (id) => {
+  const result = await Character.findByIdAndDelete(id);
+
+  return result;
+};
+
+module.exports = {
+  getAllCharacters,
+  getCharacter,
+  getCharactersByUser,
+  createCharacter,
+  editCharacter,
+  deleteCharacter,
+};
---
+++ Backend/models/characterModel.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const characterSchema = new Schema(
+  {
+    characterID: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    faceImage: {
+      type: String,
+      required: true,
+    },
+    upperBody: {
+      type: String,
+      required: true,
+    },
+    lowerBody: {
+      type: String,
+      required: true,
+    },
+    shoes: {
+      type: String,
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "usr", //
+      required: true,
+    },
+  },
+  { timestamps: true }
+).set("toJSON", {
+  transform: (document, object) => {
+    object.id = document.id;
+    delete object._id;
+    delete object.characterID;
+  },
+});
+
+const Character = mongoose.model("character", characterSchema);
+module.exports = Character;
